84165a446a445c31dc17f867f46680be3658f3fd
Connor Ballard <connorballard@hotmail.co.uk>
2016-01-13 18:30:57 +0000

    update

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #This should be a short version where i can just call functions
 
-from Functions import * #Hopefully this imports everything from the fuctions file.
+from Functions.py import * #Hopefully this imports everything from the fuctions file.
 import time
 import signal
 from neopixel import *
