5b685e022712815c8c51b275eb4afbd0f6633426
Connor Ballard <connorballard@hotmail.co.uk>
2016-02-26 18:52:05 +0000

    Update NewFunctions.py

--- NewFunctions.py
+++ NewFunctions.py
@@ -10,6 +10,7 @@ LED_BRIGHTNESS = 50      # from 0 to 255, keep it low, you don't want to be blin
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 WIDTH          = 12      # How ever many pixels wide your grid is
 HEIGHT         = 8       # How ever many pixels wide your grid is
+LED_COUNT      = 96      # Total number of LEDs
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 
